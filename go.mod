032351e8f100c45f0d8109361b94f8b2c3176b9a
Marcus Noble <github@marcusnoble.co.uk>
2021-03-21 08:42:56 +0000

    Added webpage

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
 module feed-fetcher
 
-go 1.15
+go 1.16
 
 require (
+	github.com/PuerkitoBio/goquery v1.5.1
 	github.com/gofiber/fiber/v2 v2.6.0
 	github.com/mmcdole/gofeed v1.1.0
 )
--- main.go
+++ main.go
@@ -7,13 +7,18 @@ import (
 	"os"
 	"strings"
 
+	"embed"
+
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gofiber/fiber/v2"
 	"github.com/mmcdole/gofeed"
 )
 
+//go:embed index.html
+
+var content embed.FS
+
 func main() {
-	fp := gofeed.NewParser()
 	port, ok := os.LookupEnv("PORT")
 	if !ok {
 		port = "8080"
@@ -24,56 +29,32 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		feedUrl := c.Query("url")
 		if feedUrl == "" {
-			fmt.Println("No URL provided")
-			return c.SendStatus(fiber.StatusBadRequest)
+			c.Type("html", "UTF8")
+			body, _ := content.ReadFile("index.html")
+			return c.Send(body)
 		}
 
-		_, err := fp.ParseURL(feedUrl)
-		if err != nil && err == gofeed.ErrFeedTypeNotDetected {
-			res, err := http.Get(feedUrl)
-			if err != nil {
-				fmt.Println("Failed to fetch URL")
-				return c.SendStatus(fiber.StatusInternalServerError)
+		feeds, statusCode := getFeeds(feedUrl)
+
+		if c.Is("json") {
+			if statusCode >= 400 || statusCode == 300 {
+				c.Status(statusCode)
 			}
-			defer res.Body.Close()
-			if res.StatusCode >= 400 {
-				fmt.Println("Provided URL returned an error status code")
-				return c.SendStatus(res.StatusCode)
+			return c.JSON(feeds)
+		} else {
+			c.Status(statusCode)
+			c.Location(feeds[0])
+
+			if len(feeds) > 1 {
+				responseBody := "Multiple Choices\n\n"
+				for _, feed := range feeds {
+					responseBody += feed + "\n"
+				}
+				return c.SendString(responseBody)
 			}
 
-			doc, err := goquery.NewDocumentFromReader(res.Body)
-			if err != nil {
-				fmt.Println("Failed to parse response body")
-				return c.SendStatus(fiber.StatusInternalServerError)
-			}
-
-			matches := doc.Find(`[rel="alternate"][type="application/rss+xml"]`)
-			if matches.Length() == 0 {
-				fmt.Println("No RSS feeds found on page")
-				return c.SendStatus(fiber.StatusNotFound)
-			}
-
-			foundUrl, ok := matches.First().Attr("href")
-			if !ok {
-				fmt.Println("href attribute missing from tag")
-				return c.SendStatus(fiber.StatusNotFound)
-			}
-			c.Set("Location", absoluteUrl(feedUrl, foundUrl))
-			if matches.Length() > 1 {
-				fmt.Println("Multiple feeds found on page")
-				return c.SendStatus(fiber.StatusMultipleChoices)
-			} else {
-				fmt.Println("Feed found on page")
-				return c.SendStatus(fiber.StatusTemporaryRedirect)
-			}
-		} else if err != nil {
-			fmt.Println("Failed while attempting to parse feed")
-			return c.SendStatus(fiber.StatusInternalServerError)
+			return c.Send(nil)
 		}
-
-		fmt.Println("URL provided is already a feed")
-		c.Set("Location", feedUrl)
-		return c.SendStatus(fiber.StatusMovedPermanently)
 	})
 
 	fmt.Println(app.Listen(fmt.Sprintf(":%s", port)))
@@ -86,4 +67,55 @@ func absoluteUrl(requestUrl, foundUrl string) string {
 	}
 
 	return foundUrl
+}
+
+func getFeeds(requestURL string) ([]string, int) {
+	feeds := []string{}
+
+	fp := gofeed.NewParser()
+	_, err := fp.ParseURL(requestURL)
+	if err == nil {
+		feeds = []string{requestURL}
+	} else if err != nil && err == gofeed.ErrFeedTypeNotDetected {
+		res, err := http.Get(requestURL)
+		if err != nil {
+			fmt.Println("Failed to fetch URL")
+			return feeds, fiber.StatusInternalServerError
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode >= 400 {
+			fmt.Println("Provided URL returned an error status code")
+			return feeds, res.StatusCode
+		}
+
+		doc, err := goquery.NewDocumentFromReader(res.Body)
+		if err != nil {
+			fmt.Println("Failed to parse response body")
+			return feeds, fiber.StatusInternalServerError
+		}
+
+		matches := doc.Find(`[rel="alternate"][type="application/rss+xml"]`)
+		if matches.Length() == 0 {
+			fmt.Println("No RSS feeds found on page")
+			return feeds, fiber.StatusNotFound
+		}
+
+		matches.Each(func(i int, s *goquery.Selection) {
+			feeds = append(feeds, absoluteUrl(requestURL, s.AttrOr("href", "")))
+		})
+
+		if matches.Length() > 1 {
+			fmt.Println("Multiple feeds found on page")
+			return feeds, fiber.StatusMultipleChoices
+		} else {
+			fmt.Println("Feed found on page")
+			return feeds, fiber.StatusTemporaryRedirect
+		}
+	} else if err != nil {
+		fmt.Println("Failed while attempting to parse feed")
+		return feeds, fiber.StatusInternalServerError
+	}
+
+	return feeds, 200
 }
